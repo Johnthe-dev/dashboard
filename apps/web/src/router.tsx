d730a65348735e73047ac55dfb6adeb5ae120c5a
John Johnson-Rodgers <john@johnthe.dev>
2026-05-13 06:09:25 -0600

    update

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -93,7 +93,7 @@ const routeTree = rootRoute.addChildren([
   ]),
 ])
 
-export const router = createRouter({ routeTree })
+export const router = createRouter({ routeTree, basepath: import.meta.env.BASE_URL })
 
 declare module '@tanstack/react-router' {
   interface Register {
